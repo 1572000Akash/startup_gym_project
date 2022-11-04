701f60a9412c552d9ca14e8e0b2de01a03dd8e0a
1572000Akash <akashvermageneous@gmail.com>
2022-11-04 18:52:40 +0530

    update ui

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Footer from './components/footer/Footer';
+import Hero from './components/Hero/Hero';
+import Join from './components/join/Join';
+import Plans from './components/plans/Plans';
+import Programs from './components/programs/Programs';
+import Reason from './components/reasons/Reason';
+import Testimonials from './components/testimonials/Testimonials';
+function App() {
+  return (
+    <div className="App">
+          <Hero/>
+          <Programs/>
+          <Reason/>
+          <Plans/>
+          <Testimonials/>
+          <Join/>
+          <Footer/>
+    </div>
+  );
+}
+
+export default App;
